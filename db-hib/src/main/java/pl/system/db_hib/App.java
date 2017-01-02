3b491a8eb8369f8f7b056d6ec26fbe2a4d450ac6
Agnieszka <klosak.agnieszka@gmail.com>
2017-01-02 09:33:53 +0100

    Poprawka w obsludze sesji

--- db-hib/src/main/java/pl/system/db_hib/App.java
+++ db-hib/src/main/java/pl/system/db_hib/App.java
@@ -2,6 +2,7 @@ package pl.system.db_hib;
 
 import java.util.List;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
@@ -31,30 +32,39 @@ public class App
         System.out.println( "Hibernate test" );
         
         SessionFactory sessionFactory = getSessionFactory();
+        Session session = sessionFactory.openSession();
+		
         InvoiceDAO invoiceDAO = new InvoiceDAO();
-        invoiceDAO.setSessionFactory( sessionFactory );
         
-        List<Customer> customers = invoiceDAO.getCustomers();
+        List<Customer> customers = invoiceDAO.getCustomers(session);
         for(Customer c: customers){
         	System.out.println(c.toString());
         }
         
-        List<InvoiceItem> invoiceItems = invoiceDAO.getInvoiceItems();
+        List<InvoiceItem> invoiceItems = invoiceDAO.getInvoiceItems(session);
         for(InvoiceItem i: invoiceItems){
         	System.out.println(i.toString());
         }
         
         JsonArray jA = new JsonArray();
-        List<Invoice> invoices = invoiceDAO.getInvoices();
+        List<Invoice> invoices = invoiceDAO.getInvoices(session);
         for(Invoice i: invoices){        	
         	jA.add(i.toJson());
         }
         
-        
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         String prettyJson = gson.toJson(jA);
         System.out.println(prettyJson);
-    	sessionFactory.close();
         
+        for(Customer c: customers){        	
+        	List<Invoice> invoicesBySeller = invoiceDAO.getInvoiceBySeller(session,c);
+        	for(Invoice i: invoicesBySeller){
+        		System.out.println("Invoices by customer:"+c.getName()+", id:" +c.getId() );
+        		System.out.println(gson.toJson( i.toJson() ) );
+        	}
+        }
+        session.close();
+    	sessionFactory.close();
+    	
     }
 }
--- db-hib/src/main/java/pl/system/db_hib/InvoiceDAO.java
+++ db-hib/src/main/java/pl/system/db_hib/InvoiceDAO.java
@@ -9,34 +9,29 @@ import pl.system.db_hib.entities.Invoice;
 import pl.system.db_hib.entities.InvoiceItem;
 
 public class InvoiceDAO {
-	private SessionFactory sessionFactory;
 	
-	public void setSessionFactory(SessionFactory sf){
-		this.sessionFactory = sf;
-	}
-
 	@SuppressWarnings("unchecked")
-	public List<Customer> getCustomers(){
-		Session session = sessionFactory.openSession();
-		List<Customer> customers = session.createQuery(
-        		"from "+Customer.class.getName()).list();        
-        session.close();        
-        return customers;
+	public List<Customer> getCustomers(Session session){
+		return session.createQuery(
+        		"from "+Customer.class.getName()).list();                
 	}	
+	
 	@SuppressWarnings("unchecked")
-	public List<InvoiceItem> getInvoiceItems(){
-		Session session = sessionFactory.openSession();
-		List<InvoiceItem> customers = session.createQuery(
-        		"from "+InvoiceItem.class.getName()).list();        
-        session.close();        
-        return customers;
+	public List<InvoiceItem> getInvoiceItems(Session session){
+		return session.createQuery(
+        		"from "+InvoiceItem.class.getName()).list();                
 	}
+	
 	@SuppressWarnings("unchecked")
-	public List<Invoice> getInvoices(){
-		Session session = sessionFactory.openSession();
-		List<Invoice> customers = session.createQuery(
-        		"from "+Invoice.class.getName()).list();        
-        session.close();        
-        return customers;
+	public List<Invoice> getInvoices(Session session){
+		return session.createQuery(
+        		"from "+Invoice.class.getName()).list();                
 	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Invoice> getInvoiceBySeller(Session session, Customer seller) {
+        return session.createQuery("from "+Invoice.class.getName()+" invoice where invoice.seller=?")
+                .setParameter(0, seller)
+                .list();
+    }
 }
